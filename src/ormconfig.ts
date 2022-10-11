6c9507e92a01522d3595d285d9360c8fe6421f8d
Dmitry Kanaev <dimmkan@mail.ru>
2022-10-11 08:32:07 +0300

    ormconfig fix

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,7 +3,7 @@ import { ConnectionOptions } from 'typeorm';
 
 const config: ConnectionOptions = {
     type: 'mysql',
-    host: 'localhost',
+    host: '0.0.0.0',
     port: 3306,
     username: 'root',
     password: 'password',
